ecfd3f67021d8754533faa0fefc92759d75d18af
Rolando HR <rolando167@hotmail.com>
2024-05-14 17:19:10 +0200

    RH - mod

---
+++ src/core/config/database.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const DB_USER =  process.env.DB_USER;
+const DB_PASSWORD =  process.env.DB_PASSWORD;
+const DB_HOST =  process.env.DB_HOST;
+const DB_PORT =  process.env.DB_PORT;
+const DB_NAME =  process.env.DB_NAME;
+
+const DB_CONNECTION =  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+export {
+    DB_CONNECTION
+};
