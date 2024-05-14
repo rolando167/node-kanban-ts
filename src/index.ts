e6a627394defabde4ffa562cf722a51b18a07461
Rolando HR <rolando167@hotmail.com>
2024-05-14 16:28:48 +0200

    RH - MOD

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
-import express from "express";
+ 
 import http from "http";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import cors from "cors";
-
-const app = express();
-
-app.use(cors({
-    credentials : true
-}));
-
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+ 
+// @ts-ignore
+import { app } from "./core/config/express.ts";
+ 
+ 
 
 const server = http.createServer(app);
 
